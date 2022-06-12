a89d454766dd8fde21119a98a73085f9b31f9f60
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-06-12 00:28:39 -0500

    Fixing C function that sorts an array of integers in ascending order using the bubble sort algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,11 +12,11 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t j;
 	size_t i;
-	size_t tmp;
+	int tmp;
 
-	for (j = 0; j < size; j++)
+	for (j = 0; j < size - 1; j++)
 	{
-		for (i = 0; i < size; i++)
+		for (i = 0; i < size - j - 1; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
