6054b0cb391c65688bbfb8637499ce7111d558a5
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-06-13 10:47:29 -0500

    Updating header library set (full)

--- sort.h
+++ sort.h
@@ -30,5 +30,8 @@ void quick_sort(int *array, size_t size);
 void quicksort(int *array, int low, int high, size_t size);
 int partition(int *array, int low, int high, size_t size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void swap(listint_t **head, listint_t *order_);
+void swap_2(listint_t **head, listint_t *order_);
 
 #endif /* SORT_H */
